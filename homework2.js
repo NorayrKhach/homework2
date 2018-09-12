8dea09f72121b1a390ce31d5928c639aeaea197f
NorayrKhach <42997266+NorayrKhach@users.noreply.github.com>
2018-09-12 18:25:14 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,14 @@
+<script>
+   const first = 10;
+  const second = 12;
+  const sum = first + second;
+  const div = sum/2;
+  const name = "Our best Joe Poghosyan";
+  if(div === 11){
+  console.log("This two nums sum is dvis. by 2");
+  }
+  else{
+  console.log("This numbers sum isn't even")
+  }
+    
+</script>
